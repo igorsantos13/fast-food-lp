b7f83e2c2ca8bcd125b8eee03c9bc496771a4b32
Igor Santos <igorsantos9513@outlook.com>
2023-07-04 17:56:42 -0300

    responsive layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import hero from '@/components/imagem.png'
 
-type Props = {}
+type Props = {
+  
+}
 
 function Hero({}: Props) {
   return (
-    <section className='flex items-center justify-between w-full h-screen px-10'>
+    <section className='py-24 px-[2%] flex flex-col text-center overflow-hidden gap-20 md:flex md:flex-row md:text-start md:items-center md:justify-between md:w-full md:h-screen md:px-10'>
         <div className='max-w-xl'>
             <h4 className='mb-4 text-base text-orange-500'>Welcome to Fast Food</h4>
             <h1 className='mb-4 text-6xl'>Different spice for different tastes</h1>
@@ -15,7 +17,7 @@ function Hero({}: Props) {
         </div>
 
         <div>
-            <Image src={hero} alt='Hamburger photo' width={700} height={700}/>
+            <Image className='z-0 dropShadow' src={hero} alt='Hamburger photo' width={700} height={700}/>
         </div>
     </section>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,66 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import Hero from './Hero'
 
 type Props = {}
 
 function Navbar({}: Props) {
+    const [active, setActive] = useState(false)
+
   return (
-    <nav className='fixed flex items-center justify-between w-full px-6 py-4 bg-white shadow-lg'>
+    <nav className='fixed flex items-center justify-between w-full px-4 py-2 bg-white shadow-lg md:px-6 md:py-4'>
         <h1 className='text-3xl font-bold'>Fast Food</h1>
-        <ul className='flex items-center gap-20'>
-            <li><a className='text-base font-medium hover:text-orange-400' href="#">Home</a></li>
-            <li><a className='text-base font-medium hover:text-orange-400' href="#">About</a></li>
-            <li><a className='text-base font-medium hover:text-orange-400' href="#">Menu</a></li>
-            <li><a className='text-base font-medium hover:text-orange-400' href="#">Contact</a></li>
+
+        {/* SE CLICADO!! */}
+        {active ? (
+            <ul className='fixed z-10 right-0 flex flex-col items-center justify-around w-full h-full bg-white top-16 gap-[-100px]'>
+            <MagnifyingGlassIcon className='w-8 h-8 cursor-pointer'/>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Home</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">About</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Menu</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Contact</a></li>
+
+        </ul>
+        ) : (
+
+            // padrao sem ter clicado
+            <ul className='hidden md:flex md:items-center md:gap-20'>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Home</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">About</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Menu</a></li>
+            <li><a className='mx-4 my-0 md:text-base md:font-medium hover:text-orange-400' href="#">Contact</a></li>
             <MagnifyingGlassIcon className='w-8 h-8 cursor-pointer'/>
 
         </ul>
+        )}
 
-        <div className='cursor-pointer'>
+        
+
+       
+            {active ? (
+                <div className='block cursor-pointer ' onClick={()=>setActive(false)}>
+                <span className='block h-1 m-auto my-1 bg-orange-600 w-7 translate-y-[8px] rotate-[45deg]'></span>
+                <span className='block h-1 m-auto my-1 bg-black opacity-0 w-7'></span>
+                <span className='block h-1 m-auto my-1 bg-orange-600 w-7 translate-y-[-8px] rotate-[-45deg]'></span>
+                </div>
+            ) : (
+
+                //SEM TER CLICADO AINDA
+                <div className='md:hidden md:cursor-pointer' onClick={()=> setActive(true)}>
             <span className='block h-1 m-auto my-1 bg-orange-600 w-7'></span>
             <span className='block h-1 m-auto my-1 bg-black w-7'></span>
             <span className='block h-1 m-auto my-1 bg-orange-600 w-7'></span>
         </div>
+            )
+        }
+        
+        
+            
+        
+        
+       
+        
     </nav>
   )
 }
